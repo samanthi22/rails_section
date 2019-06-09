7d8ff8ef125d69e3bf9516e0c94a83ac05956ce7
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2019-06-09 18:53:09 +0000

    added #show

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,6 +2,11 @@ class Cat < ActiveRecord::Base
     validates :name, presence: true
     
     def index
+        cats = Cat.all
+        self.render json: cats
+    end
     
+    def show
+        self.render json: Cat.find(self.params)
     end
 end 
