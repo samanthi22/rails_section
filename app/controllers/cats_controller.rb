fb627eb96ae0f14d6816039ddd0607ba904b0592
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2019-06-09 19:00:34 +0000

    Previewed on localhost:3000/cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,3 +1,14 @@
 class CatsController < ApplicationController
+    def index
+        cats = Cat.all
+        self.render json: cats
+    end
     
+    def show
+        self.render json: Cat.find(self.params[:id])
+    end
+    
+    def create
+    
+    end
 end
