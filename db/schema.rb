7d9ba210b9a606489724872bd3716c5c8f122841
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2019-06-30 22:41:41 +0000

    Added skill to cats

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_200242) do
+ActiveRecord::Schema.define(version: 2019_06_30_223431) do
 
   create_table "cats", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "skill"
   end
 
   create_table "toys", force: :cascade do |t|
